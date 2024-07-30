ac4efcea59af41fa34f3cb1df027a5ad087a7f50
tienanh1409.work <tienanh1409.work@gmail.com>
2024-07-30 18:17:08 +0700

    fix

--- controller/chat.py
+++ controller/chat.py
@@ -1,12 +1,7 @@
-from fastapi import Depends, APIRouter, HTTPException, status
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, HTTPException
 import requests
-from database import get_db
 from schema.chat import ChatMessage
-from schema.token import Token
-from schema.user import UserLogin
-from service import user as user_service
-from service import auth as auth_service
+
 from config.setting import MODEL_ENDPOINT
 
 router = APIRouter(
@@ -22,5 +17,4 @@ def send_message(chat_message: ChatMessage) -> str:
         response.raise_for_status()  # Check if the request was successful
         return response.json()
     except requests.RequestException as e:
-        raise HTTPException(status_code=500, detail=str(e))us
-        g
+        raise HTTPException(status_code=500, detail=str(e))
